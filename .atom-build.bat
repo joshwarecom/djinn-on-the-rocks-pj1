fb9b3fc7da14cee0b1f1b6f111e73c1e98a7b48e
joshwarecom <josh@joshware.com>
2021-12-18 09:38:53 -0500

    updating to latest version of punyinform; code cleanup;
    Signed-off-by: joshwarecom <josh@joshware.com>

--- .atom-build.bat
+++ .atom-build.bat
@@ -2,7 +2,7 @@
 CD %~dp0
 CLS
 
-"e:\tools\inform\inform6.exe" -c +"PunyInform\lib" "main.inf" -v3
+"e:\tools\inform\inform6.exe" -c +"PunyInform\lib" "src\main.inf" -v3
 IF ERRORLEVEL 1 PAUSE & EXIT
 
 START "" main.z3
